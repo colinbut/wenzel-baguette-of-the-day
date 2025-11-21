7e5bb27282c8a2786725db8569a50a558852ff39
Colin But <colin.but@hotmail.co.uk>
2025-11-21 09:15:54 +0000

    chore: cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import Baguette from './Baguette.svelte';
-	//import welcome from '$lib/images/svelte-welcome.webp';
-	//import welcomeFallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
@@ -21,33 +19,5 @@
 		align-items: center;
 		flex: 0.6;
 	}
-
-	/* h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-
-	.baguette-text-bg-panel {
-  		border-radius: 25px;
-  		background-color: #540884;
-  		padding: 20px;
-  		width: 200px;
-  		height: 10px;
-		color: white;
-	} */
+	
 </style>
